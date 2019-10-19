7d324c702dcf0c93e21c5e3f38865a125e018141
Thien Chi Vi <meomeocf98@gmail.com>
2019-10-19 12:09:14 +0700

    Update dotfiles (#1)

--- run.sh
+++ run.sh
@@ -49,8 +49,10 @@ function install_zsh_base {
 	echo 'install zsh base'
 	sudo apt install -y zsh
 	sudo chsh -s $(which zsh)
-    	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-    	git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+	yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+	git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+
 }
 
 #update_packages
